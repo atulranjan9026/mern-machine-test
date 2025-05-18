3ea70d15eb14081a93c3a711106fe317381e942b
Atul <99653239+atulranjan9026@users.noreply.github.com>
2025-05-19 02:58:15 +0530

    babel

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,19 +1,12 @@
 module.exports = {
-  webpack: {
-    configure: (webpackConfig) => {
-      webpackConfig.module.rules.push({
-        test: /\.(js|mjs|jsx|ts|tsx)$/,
-        include: /node_modules/,
-        loader: require.resolve('babel-loader'),
-        options: {
-          presets: ['@babel/preset-env', '@babel/preset-react'],
-          plugins: [
-            '@babel/plugin-proposal-nullish-coalescing-operator',
-            '@babel/plugin-proposal-optional-chaining'
-          ]
-        }
-      });
-      return webpackConfig;
-    }
+  babel: {
+    presets: [
+      '@babel/preset-env',
+      '@babel/preset-react'
+    ],
+    plugins: [
+      '@babel/plugin-proposal-optional-chaining',
+      '@babel/plugin-proposal-nullish-coalescing-operator'
+    ]
   }
 };
